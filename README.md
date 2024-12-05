91b0a549e0ed07edf3af59146e5b125d7cc2d29d
kunalpawar83 <kunalpawar8319@gmail.com>
2024-12-06 01:03:13 +0530

    connect to data base

---
+++ README.md
@@ -0,0 +1,1 @@
+https://hotel-tgs-git-main-kunal-pawars-projects-3d399083.vercel.app/auth/login
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,4 +5,8 @@ router.get("/login", (req, res) => {
   res.send("Login page");
 });
 
+router.get("/", (req, res) => {
+  res.send("Home page");
+});
+
 module.exports = router;

a4d51cc3b2072827b0f9c9d888cadf059d9ddd73
kunalpawar83 <kunalpawar8319@gmail.com>
2024-12-06 00:56:08 +0530

    connect to data base

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const connectDb = require("./utils/connectDB");
 
 const authRoute = require("./routes/authRoute");
 const cors = require("cors");
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require("http");
+const connectDb = require("./utils/connectDB");
 const app = require("./app.js");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+connectDb().then(() => {
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });

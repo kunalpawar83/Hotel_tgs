5baff35f45e9ef3654e7ba9b54eaff1c0453cfbf
kunalpawar83 <kunalpawar8319@gmail.com>
2024-12-06 00:35:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const authRoute = require("./routes/authRoute");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/auth", authRoute);
+
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "404 Not Found" });
+});
+
+module.exports = app;
